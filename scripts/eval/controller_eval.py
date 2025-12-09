f2ce143465dd199e8c6d5a671fad59686cb93b9a
junjie wang <1697062448@qq.com>
2025-12-09 17:56:15 +0800

    update

--- scripts/eval/controller_eval.py
+++ scripts/eval/controller_eval.py
@@ -41,25 +41,20 @@ def main():
         num_envs=1,
     )
     controller = SE3Control(quad_params)
-    # runner = OnPolicyRunner(track_task, train_config, "", device="cuda:0")
-    # runner.load("logs/track_rl/policy_demo/model_500.pt")
-    # policy = runner.get_inference_policy(device="cuda:0")
     obs = track_task.reset()    # tensordict
 
     with torch.no_grad():
         for step in range(max_sim_step):
-            wxyz_quat = genesis_env.drone.odom.body_quat.cpu().numpy().flatten()  # 获取 [w, x, y, z] 格式的四元数
+            wxyz_quat = genesis_env.drone.odom.body_quat.cpu().numpy().flatten()  
             print(genesis_env.drone.odom.world_pos)
             xyzw_quat = wxyz_quat[1:].tolist() + [wxyz_quat[0]]
                 
-            # 读取当前无人机状态
             state = {
             "x": genesis_env.drone.odom.world_pos.cpu().numpy().flatten(),
             "v": genesis_env.drone.odom.world_linear_vel.cpu().numpy().flatten(),
             "q": xyzw_quat,
             "w": genesis_env.drone.odom.body_ang_vel.cpu().numpy().flatten()
             }
-            #genesis_env.drone.odom.world_pos
             flat= {
             "x": track_task.command_buf.squeeze().tolist(),
             "x_dot": [0, 0, 0],
@@ -68,19 +63,12 @@ def main():
             "yaw": 0.0,
             "yaw_dot": 0.0
             }
-            # 求控制输入
             ctrl = controller.update(0, state, flat)
             min_t = 0  
             max_t =25
             thrust_norm = (ctrl["cmd_thrust"] - min_t) / ((max_t - min_t))
             thrust_norm = thrust_norm * 2 - 1
-            # 姿态归一化
-            #thrust_norm=0.7
-            eulers = R.from_quat(ctrl["cmd_q"]).as_euler("xyz")
-            #print("eulers:", eulers)
-            eulers_norm = eulers / np.pi 
             wx, wy, wz = ctrl["cmd_w"]   # 期望机体系角速度，单位 rad/s
-            action2 = np.hstack([eulers, thrust_norm]).reshape(1, -1)
             roll_norm  = wx / 10
             pitch_norm = wy / 10
             yaw_norm   = wz / 7
@@ -92,8 +80,6 @@ def main():
             # # -------------------------
             action = np.hstack([roll_norm, pitch_norm, yaw_norm,thrust_norm ]).reshape(1, -1)
             action_tensor = torch.from_numpy(action).to(device).float()
-            print("action:", action)
-            #print("action:", wxyz_quat)
 
             obs, rews, dones, infos = track_task.step(action_tensor)
 
