3dcaa8f13a25ba5f382d7027d5960daf05ed0058
junjie wang <1697062448@qq.com>
2026-02-04 21:54:55 +0800

    clear Track_task

--- scripts/eval/rc_FPV_eval.py
+++ scripts/eval/rc_FPV_eval.py
@@ -8,7 +8,6 @@ from genesis_drones.controllers import PIDcontroller
 from genesis_drones.sensors.odom import Odom
 from genesis_drones.utils.mavlink_rc import start_mavlink_receive_thread
 from genesis_drones.envs.genesis_env import Genesis_env
-from algorithms.rl.tasks.track_task import Track_task
 from rsl_rl.runners import OnPolicyRunner
 import time
 from datetime import datetime
